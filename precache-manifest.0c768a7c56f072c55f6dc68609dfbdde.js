fdd8e36df0abb7bbd8624771e3320b37de846822
Elis Offerni <elis@eliss-mbp.lan>
2020-04-15 18:14:27 -0700

    New website

---
+++ precache-manifest.0c768a7c56f072c55f6dc68609dfbdde.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "110ed0b81b715454b587f171645532bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d6122fe6896b72f9315",
+    "url": "/static/css/main.fedc7df1.chunk.css"
+  },
+  {
+    "revision": "682ea9f754fcf65492af",
+    "url": "/static/js/2.fe48bc37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fe48bc37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d6122fe6896b72f9315",
+    "url": "/static/js/main.83d6a3d1.chunk.js"
+  },
+  {
+    "revision": "3d5fd2a8b9e99894289b",
+    "url": "/static/js/runtime-main.1855bbcb.js"
+  },
+  {
+    "revision": "da468188e5ff6d41acfd30ba8596bf21",
+    "url": "/static/media/ginger-cat-735.da468188.png"
+  },
+  {
+    "revision": "e65a81e4da13171997518b4b2ae72a74",
+    "url": "/static/media/ginger-cat-759.e65a81e4.png"
+  }
+]);
